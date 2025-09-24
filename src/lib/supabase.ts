88863242b72eee173cb95762c7d543202279942f
sws121-logo <83629055+sws121-logo@users.noreply.github.com>
2025-09-24 15:57:34 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://eqggjcphcvkocuphrach.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxZ2dqY3BoY3Zrb2N1cGhyYWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MDgzNjIsImV4cCI6MjA3NDI4NDM2Mn0.i6Dn0RZ9iinZGc72SjhiQ-aABAKxtebz35I6R6eVa5E';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
